fd6db97f028701361f91e8e16f8bb571471565d1
desperadoccy <527707607@qq.com>
2020-05-05 14:42:08 +0800

    完成登陆界面

---
+++ src/components/Home.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <el-button type="infor" @click="logout">退出</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    logout () {
+      window.sessionStorage.removeItem('token')
+      this.$message.success('注销成功')
+      this.$router.push('/login')
+    }
+  }
+}
+</script>
+
+<style lang="less" scoped>
+</style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,13 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 
+// 导入全局样式表
+import './assets/css/global.css'
+
+// 导入axios
+import axios from 'axios'
+Vue.prototype.$http = axios
+
 Vue.config.productionTip = false
 
 new Vue({
